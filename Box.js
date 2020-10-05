51edd1a6684d729d108cac9669a35d56dcd2bfc3
SyngyeonTak <67184044+SyngyeonTak@users.noreply.github.com>
2020-10-05 17:47:12 +0900

    c

---
+++ Box.js
@@ -0,0 +1,25 @@
+/*충돌체크를 학습하기 위한 박스 객체 정의*/
+class Box{
+    constructor(bg, x, y){
+        this.div = document.createElement("div");
+        this.bg = bg;//인스턴스의 색상 결정
+        this.x = x;//인스턴스의 x좌표 결정
+        this.y = y;//인스턴스의 y좌표 결정
+        this.width = 100;
+        this.height = 100;
+        this.div.style.width = this.width+"px";
+        this.div.style.height = this.height+"px";
+        this.div.style.background = this.bg;
+        this.div.style.color = "white";
+
+        this.div.style.position = "absolute";
+        this.div.style.left = this.x+"px";
+        this.div.style.top = this.y+"px";
+
+        document.body.appendChild(this.div);//화면에 부착!!
+    }
+
+    set(bg){
+        this.div.style.background = bg;
+    }
+}
